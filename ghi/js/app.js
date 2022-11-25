3517d61e5ddd06e5c9ef0eee3fa9f4569cc7092e
Noah Maurer <noahmaurer12@gmail.com>
2022-11-25 11:35:11 -0600

    third page exploratoin

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -77,14 +77,3 @@ window.addEventListener("DOMContentLoaded", async () => {
         error.innerHTML += html;
     }
 });
-
-window.addEventListener("DOMContentLoaded", async () => {
-    const url = "http://localhost:8000/api/states/";
-
-    const response = await fetch(url);
-
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-    }
-});
